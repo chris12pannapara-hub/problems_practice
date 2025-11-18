6cca55466a3ceaf089e9cd4dccf0a3e8faa76399
Chris <chris12pannapara@gmail.com>
2025-11-17 22:02:47 -0500

    Adding some changes

--- Wordbreak.js
+++ Wordbreak.js
@@ -1,7 +1,9 @@
 //Problem: Given a string and a word dictionary, determine if string can be segmented into space-separated words from dictionary.
 function wordBreak(s, wordDict) {
     const wordSet = new Set(wordDict);
+    console.log(wordSet);
     const dp = Array(s.length + 1).fill(false);
+    console.log(s.length); 
     dp[0] = true;
     for (let i = 1; i <= s.length; i++) {
         for (let j = 0; j < i; j++) {
